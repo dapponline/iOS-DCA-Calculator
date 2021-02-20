d43193baeffdae35da37b493b6115c1c84f528f9
D APP ONLINE LTD <dapp.online@gmail.com>
2021-02-20 18:40:03 +0000

    Added SearchTableViewCell

---
+++ iOS-DCA-Calculator/View/SearchTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchTableViewCell.swift
+//  iOS-DCA-Calculator
+//
+//  Created by Leon Smith on 20/02/2021.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    @IBOutlet weak var assetNameLabel:UILabel!
+    @IBOutlet weak var assetSymbolLabel:UILabel!
+    @IBOutlet weak var assetTypeLabel:UILabel!
+    
+}
