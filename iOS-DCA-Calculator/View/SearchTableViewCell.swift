c2ee16ae80c5efe2024838cb3e890254f7852620
D APP ONLINE LTD <dapp.online@gmail.com>
2021-02-20 18:53:19 +0000

    Created test API Keys

--- iOS-DCA-Calculator/View/SearchTableViewCell.swift
+++ iOS-DCA-Calculator/View/SearchTableViewCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class SearchTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var assetNameLabel:UILabel!
     @IBOutlet weak var assetSymbolLabel:UILabel!
     @IBOutlet weak var assetTypeLabel:UILabel!
